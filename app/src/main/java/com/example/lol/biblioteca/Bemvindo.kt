6966d84bff415a7d76abf2952bd5aac4d6334bae
Simplim <lucas.lima.nave@gmail.com>
2019-05-27 09:53:54 -0300

    classes das activity

---
+++ app/src/main/java/com/example/lol/biblioteca/Bemvindo.kt
@@ -0,0 +1,13 @@
+package com.example.lol.biblioteca
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Bemvindo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bemvindo_activity)
+    }
+}
+
